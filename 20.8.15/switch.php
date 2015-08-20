91b718ff79a1521fbfc91d96d87b08eaede6970a
mahmudul-islam32 <mahmud.islam32@gmail.com>
2015-08-21 03:48:55 +0600

    20.8.15

---
+++ 20.8.15/switch.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+switch (2)
+{
+case 1:
+  echo "Number 1";
+  break;
+case 2:
+  echo "Number 2";
+  break;
+case 3:
+  echo "Number 3";
+  break;
+default:
+  echo "No number between 1 and 3";
+}
+
+
+  $fuel = 10;
+  while ($fuel > 1)
+  {
+    // Keep driving ...
+    echo "There's enough fuel";
+  }
+
+
+?>
+
+</body>
+</html>
